97d12b2fcc492fe7b65d3b1c17af284597816e79
Tigran Yeritsyan <36622523+YeritsyanTigran@users.noreply.github.com>
2019-10-16 02:02:52 +0400

    Update JsonFilter.java

--- sanitizer/src/main/java/com/TigranIncorporation/sanitizer/config/JsonFilter.java
+++ sanitizer/src/main/java/com/TigranIncorporation/sanitizer/config/JsonFilter.java
@@ -12,7 +12,7 @@ public class JsonFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         ResponseWrapper wrapper = new ResponseWrapper((HttpServletResponse) response);
         chain.doFilter(request,wrapper);
-        if(response.getContentType().contains("application/json")) {
+        if(response.getContentType() == null || response.getContentType().contains("application/json")) {
             String content = new String(wrapper.getDataStream());
             content = JsonSanitizer.sanitize(content);
             response.getOutputStream().write(content.getBytes());
